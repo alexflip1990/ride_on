1aee71c2a3939423aaf169730d5fa120214f52e1
alexflip1990 <>
2024-01-29 19:34:44 +0000

    added toasts messages

--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
+from products.models import Product
 
 
 
@@ -14,18 +15,18 @@ def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
 
     quantity = int(request.POST.get('quantity'))
-    #products = get_object_or_404(Product, pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
-        #messages.success(
-            #request, f'Updated {product.name}  quantity to {basket[item_id]}'
-        #)
+        messages.success(
+            request, f'Updated {product.name} quantity to {basket[item_id]}'
+        )
     else:
         basket[item_id] = quantity
-        #messages.success(request, f'Added {product.name} to your basket')
+        messages.success(request, f'Added {product.name} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
