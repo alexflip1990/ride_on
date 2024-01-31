5d60fb2f744b709771acc9f0e2056ad60b10ba67
alexflip1990 <>
2024-01-31 13:30:11 +0000

    added checkout form data caching in payment intent

--- ride_on/settings.py
+++ ride_on/settings.py
@@ -178,6 +178,9 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+print(STRIPE_PUBLIC_KEY)
+print(STRIPE_SECRET_KEY )
+print(STRIPE_WH_SECRET)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
