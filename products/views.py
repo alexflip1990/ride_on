58cbc0c1c35cd3da88d44b2fbc7084e804677170
alexflip1990 <>
2024-02-06 13:40:06 +0000

    Product admin - fixing the image field part 2

--- products/views.py
+++ products/views.py
@@ -128,7 +128,7 @@ def edit_product(request, product_id):
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
-            messages.succes(request, 'Successfully updated product!')
+            messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to update product. Please ensure the form is valid.')
