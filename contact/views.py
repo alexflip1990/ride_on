ff3e9213d0961503d6fd5caefc00e03748e5ec06
alexflip1990 <>
2024-02-06 15:37:46 +0000

    Contact views completed

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from .forms import CustomerContactForm
+from profiles.models import UserProfile
 
-# Create your views here.
+from django.contrib import messages
+
+
+def contact(request):
+
+    if request.method == 'POST':
+        contact_form = CustomerContactForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.success(request, 'Message sent successfully.')
+            return render(request, 'contact/contact_success.html')
+        else:
+            messages.error(request, 'An error occured sending your message. \
+            Please ensure all fields are valid.')
+            return redirect('contact')
+
+
+    else:
+        if request.user.is_authenticated:
+            try:
+                user = UserProfile.objects.get(user=request.user)
+                contact_form = CustomerContactForm(initial={
+                    'contact_name': user.default_full_name,
+                    'contact_email': user.user.email,
+                    'contact_phone_number': user.default_phone_number,
+                })
+            except UserProfile.DoesNotExist:
+                contact_form = CustomerContactForm()
+        else:
+            contact_form = CustomerContactForm()
+
+    template = 'contact/contact.html'
+    context = {
+        'form': contact_form,
+    }
+
+    return render(request, template, context)
