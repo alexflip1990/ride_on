2cfcfcf48e8f747263b21c816d45ea15d3f8e86a
alexflip1990 <>
2024-02-09 18:23:53 +0000

    Added views for subscribe app

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from .models import Newsletter
+from .forms import SubscribeForm
 
-# Create your views here.
+
+def new_subscriber(request):
+    """ Adds a new email to the subscriber list"""
+
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data['email']
+
+            if Newsletter.objects.filter(email=email).exists():
+                messages.error(
+                    request, f"{email} already exists. Please enter email again.")
+            else:
+                form.save()
+                messages.success(
+                    request, f"{email} has been successfully added.")
+    else:
+        form = SubscribeForm(None)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
