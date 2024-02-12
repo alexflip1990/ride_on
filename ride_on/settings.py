e3885020c29b0cb0b49e229f875a9d86038bfd97
alexflip1990 <>
2024-02-12 18:27:07 +0000

    Fixed indentation error in settings.py

--- ride_on/settings.py
+++ ride_on/settings.py
@@ -221,7 +221,7 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'rideon@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = = True
+    EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
