cd2f3e516e98df4498c22c53ff7278e7cd24f6fe
alex flipping <alex_flipping@hotmail.co.uk>
2024-02-26 19:07:41 +0000

    fixed product search error

--- products/views.py
+++ products/views.py
@@ -48,7 +48,7 @@ def all_products(request):
 
         # Handles search query
         if 'q' in request.GET:
-            Query = request.GET['q']
+            query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
