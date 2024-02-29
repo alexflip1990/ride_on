9ae1f89d7dc9cb7549693f3f7b6f20e2d32d67a2
alex flipping <alex_flipping@hotmail.co.uk>
2024-02-29 23:47:05 +0000

    Amending indentation part 1

--- basket/views.py
+++ basket/views.py
@@ -4,12 +4,11 @@ from django.contrib import messages
 from products.models import Product
 
 
-
-
 def view_basket(request):
     """ A view that renders the basket contents page """
 
     return render(request, 'basket/basket.html')
+
 
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
@@ -33,7 +32,9 @@ def add_to_basket(request, item_id):
 
 
 def adjust_basket(request, item_id):
-    """ Adjust the quantity of the specified product to the specified amount """
+    """
+    Adjust the quantity of the specified product to the specified amount
+    """
 
     quantity = int(request.POST.get('quantity'))
     product = get_object_or_404(Product, pk=item_id)
@@ -41,7 +42,8 @@ def adjust_basket(request, item_id):
 
     if quantity > 0:
         basket[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
+        messages.success(request, f'Updated {product.name} \
+                         quantity to {basket[item_id]}')
     else:
         basket.pop(item_id)
         messages.success(request, f'Removed {product.name} from your basket')
@@ -52,7 +54,7 @@ def adjust_basket(request, item_id):
 
 def remove_from_basket(request, item_id):
     """ Remove the item from the shopping basket """
-    
+
     try:
         product = get_object_or_404(Product, pk=item_id)
         basket = request.session.get('basket', {})
@@ -61,7 +63,7 @@ def remove_from_basket(request, item_id):
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
-    
+
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
