cfa8c906aa057f2ee5c96bd060ef23943120f9a4
alex flipping <alex_flipping@hotmail.co.uk>
2024-02-29 12:29:06 +0000

    Added container div to delivery template

--- contact/urls.py
+++ contact/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.contact, name='contact'),
+    path('delivery/', views.delivery, name='delivery'),
     path('t_and_c/', views.t_and_c, name='t_and_c'),
     path('privacy/', views.privacy, name='privacy'),
-    path('delivery/', views.delivery, name='delivery'),
+    
 ]
